f991172d07a67d4ddd541b58e1ed06823a65f200
Oswe-gif <jhoanoswaldo72@gmail.com>
2025-10-04 11:31:47 -0500

    add folders

---
+++ Intermediate/closures.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func main() {
+	sequence := adder()
+	fmt.Println(sequence())
+	fmt.Println(sequence())
+	fmt.Println(sequence())
+	fmt.Println(sequence())
+
+	sequence2 := adder()
+	fmt.Println(sequence2())
+	fmt.Println(sequence2())
+	fmt.Println(sequence2())
+	fmt.Println(sequence2())
+
+	subtracter := func ()func(int)int  {
+		countdown := 99
+		return func(i int) int {
+			countdown -= i
+			return countdown
+		}
+	}()
+	fmt.Println(subtracter(99))
+}
+
+func adder() func() int {
+	i := 0
+	return func() int {
+		i++
+		return i
+	}
+}
